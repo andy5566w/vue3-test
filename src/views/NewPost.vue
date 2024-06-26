17a8e005588967e6b9d8e56debbc5619d915a857
andy.tsai <andy5566w@gmail.com>
2024-06-26 15:23:07 +0800

    2-26

--- src/views/NewPost.vue
+++ src/views/NewPost.vue
@@ -1,5 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PostWriter from '@/components/PostWriter.vue'
+import { TimelinePost } from '@/posts'
+import { DateTime } from 'luxon'
 
-<template>New Post</template>
+const post: TimelinePost = {
+  id: -1,
+  title: 'Title',
+  created: DateTime.now(),
+}
+</script>
+
+<template><PostWriter :post="post" /></template>
 
 <style scoped></style>
